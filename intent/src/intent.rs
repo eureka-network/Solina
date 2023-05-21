a8a5f7617ecc1f13019a73c95950db09eebfaaa1
jorgeantonio21 <matroid@outlook.com>
2023-05-21 13:01:06 +0100

    add intent interface

--- intent/src/intent.rs
+++ intent/src/intent.rs
@@ -1,8 +1,28 @@
-use crate::{StructuredHash, C};
-use num_bigint::BigUint;
-use plonky2_ecdsa::curve::ecdsa::ECDSASignature;
+use crate::StructuredHash;
+use plonky2::field::secp256k1_scalar::Secp256K1Scalar;
+use plonky2_ecdsa::curve::{
+    ecdsa::{ECDSASecretKey, ECDSASignature},
+    secp256k1::Secp256K1,
+};
+
+/// Reference enum from a label to the actual
+/// computation execution, and a proof generation
+pub(crate) enum ExecuteRuntime {
+    Swap,
+}
 
 pub(crate) trait Intent {
+    type Inputs;
+    type Constraints;
+
+    fn build_intent(
+        inputs: Self::Inputs,
+        constraints: Self::Constraints,
+        execute_runtime: ExecuteRuntime,
+    ) -> Self;
     fn structured_hash(&self) -> StructuredHash;
-    // fn sign_intent(&self, private_key: BigUint) -> ECDSASignature<C>;
+    fn sign_intent(&self, private_key: ECDSASecretKey<Secp256K1>) -> ECDSASignature<Secp256K1>;
+    fn get_constraints(&self) -> Self::Constraints;
+    fn get_inputs(&self) -> Self::Inputs;
+    fn get_runtime_execution(&self) -> ExecuteRuntime;
 }
--- intent/src/swap_intent.rs
+++ intent/src/swap_intent.rs
@@ -1,21 +1,77 @@
-use plonky2_ecdsa::gadgets::biguint::BigUintTarget;
-use crate::StructuredHash;
-/// Intent to swap tokens 
-/// todo[ben]: this is incomplete, but let's focus on the pathways first
+use crate::{
+    intent::{ExecuteRuntime, Intent},
+    StructuredHash,
+};
+use num_bigint::BigUint;
+use plonky2::field::{secp256k1_scalar::Secp256K1Scalar, types::Field};
+use plonky2_ecdsa::{
+    curve::{
+        ecdsa::{sign_message, ECDSASecretKey, ECDSASignature},
+        secp256k1::Secp256K1,
+    },
+    gadgets::biguint::BigUintTarget,
+};
 
-#[allow(dead_code)]
-pub struct SwapIntent {
+/// Inputs for a swap
+#[derive(Clone, Debug)]
+pub(crate) struct SwapInputs {
     /// address
-    from: BigUintTarget,
-    /// address
-    quote_token: BigUintTarget,
-    /// address
-    base_token: BigUintTarget,
-    /// in Wei
-    quote_amount: BigUintTarget,
-    /// in Wei
-    base_amount: BigUintTarget,
+    from: BigUint,
+    /// quote token
+    quote_token: BigUint,
+    /// base token
+    base_token: BigUint,
+    /// quote amount
+    quote_amount: BigUint,
 }
 
+/// Constraints for a swap
+#[derive(Clone, Debug)]
+pub(crate) struct SwapConstraints {
+    /// max slippage amount
+    min_base_token_amount: BigUint,
+}
 
-impl 
+/// Intent to swap tokens
+/// todo[ben]: this is incomplete, but let's focus on the pathways first
+#[allow(dead_code)]
+pub struct SwapIntent {
+    inputs: SwapInputs,
+    constraints: SwapConstraints,
+}
+
+impl Intent for SwapIntent {
+    type Inputs = SwapInputs;
+    type Constraints = SwapConstraints;
+
+    fn build_intent(
+        inputs: Self::Inputs,
+        constraints: Self::Constraints,
+        _execute_runtime: crate::intent::ExecuteRuntime,
+    ) -> Self {
+        Self {
+            inputs,
+            constraints,
+        }
+    }
+
+    fn get_constraints(&self) -> Self::Constraints {
+        self.constraints.clone()
+    }
+
+    fn get_inputs(&self) -> Self::Inputs {
+        self.inputs.clone()
+    }
+
+    fn get_runtime_execution(&self) -> ExecuteRuntime {
+        ExecuteRuntime::Swap
+    }
+
+    fn sign_intent(&self, private_key: ECDSASecretKey<Secp256K1>) -> ECDSASignature<Secp256K1> {
+        let message = self.structured_hash();
+        let message = Secp256K1Scalar::from_noncanonical_biguint(BigUint::from_bytes_le(&message));
+        sign_message(message, private_key)
+    }
+
+    fn structured_hash(&self) -> StructuredHash {}
+}
