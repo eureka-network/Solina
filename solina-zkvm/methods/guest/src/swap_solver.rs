67c3eef2ffa8549127ce430f8b438425f1cba5ae
jorgeantonio21 <matroid@outlook.com>
2023-06-20 10:48:27 +0100

    swap solver interface

---
+++ solina-zkvm/methods/guest/src/swap_solver.rs
@@ -0,0 +1,22 @@
+use crate::{swap_challenger::SwapSolution, swap_intent::SwapIntent, PublicKey};
+use solina::solver::Solver;
+
+pub struct SwapSolver {}
+
+pub struct SwapSolutionTrace {}
+
+impl Solver<SwapIntent> for SwapSolver {
+    type Solution = SwapSolution;
+    type SolutionTrace = SwapSolutionTrace;
+    type Addr = PublicKey;
+
+    fn publish_solution(&self) -> Self::Solution {
+        todo!()
+    }
+    fn publish_solution_trace(&self) -> Self::SolutionTrace {
+        todo!()
+    }
+    fn get_address(&self) -> Self::Addr {
+        todo!()
+    }
+}
